703d1e888049f32dc0cc77d00dd8a76fde0b388d
wolfmoonhust <wolfmoon.hust@gmail.com>
2020-07-16 22:45:14 +0700

    lesson-02

---
+++ app/src/main/java/wolfdev01/com/demowithgit/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package wolfdev01.com.demowithgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}

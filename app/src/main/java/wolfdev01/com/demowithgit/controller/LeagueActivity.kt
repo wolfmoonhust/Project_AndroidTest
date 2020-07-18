7061034e7af0567fff087954b75583a45b554a68
wolfmoonhust <wolfmoon.hust@gmail.com>
2020-07-19 00:28:13 +0700

    send parcelable, make icon

--- app/src/main/java/wolfdev01/com/demowithgit/controller/LeagueActivity.kt
+++ app/src/main/java/wolfdev01/com/demowithgit/controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package wolfdev01.com.demowithgit.controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
@@ -16,6 +17,20 @@ class LeagueActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    override fun  onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
+
 
     fun leagueNextClicked(view: View) {
 
--- app/src/main/java/wolfdev01/com/demowithgit/controller/SkillActivity.kt
+++ app/src/main/java/wolfdev01/com/demowithgit/controller/SkillActivity.kt
@@ -19,6 +19,18 @@ class SkillActivity : BaseActivity() {
         println()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player =  savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
     fun onFinishSkillClicked(view: View) {
         if (player.skil != "") {
             val finishActivity = Intent(this, FinishActivity::class.java)

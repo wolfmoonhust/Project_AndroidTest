b4509a99cd0e4685355a4f7035f06fec64db4ad2
wolfmoonhust <wolfmoon.hust@gmail.com>
2020-07-23 00:38:22 +0700

    add custom adapter

--- app/src/main/java/wolfdev01/com/demowithgit/ListViewActivity.kt
+++ app/src/main/java/wolfdev01/com/demowithgit/ListViewActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_list_view.*
+import wolfdev01.com.demowithgit.adapter.CategoryAdapter
 import wolfdev01.com.demowithgit.services.DataServices
 
 class ListViewActivity : AppCompatActivity() {
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,DataServices.categories)
+        adapter = CategoryAdapter(this, DataServices.categories)
         categoryList.adapter = adapter
     }
 }
--- app/src/main/java/wolfdev01/com/demowithgit/services/DataServices.kt
+++ app/src/main/java/wolfdev01/com/demowithgit/services/DataServices.kt
@@ -8,7 +8,15 @@ object DataServices {
             Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
-            Category("DIGITAL", "degitalgoodsimage")
+            Category("DIGITAL", "digitalgoodsimage"),
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodieimage"),
+            Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage"),
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodieimage"),
+            Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf<Product>(

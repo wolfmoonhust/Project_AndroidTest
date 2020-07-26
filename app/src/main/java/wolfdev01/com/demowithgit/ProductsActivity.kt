7eebcdff91d1a1a205e20bd8dbbf96e668ec895e
wolfmoonhust <wolfmoon.hust@gmail.com>
2020-07-26 11:10:36 +0700

    finish implement for ProductActivity

--- app/src/main/java/wolfdev01/com/demowithgit/ProductsActivity.kt
+++ app/src/main/java/wolfdev01/com/demowithgit/ProductsActivity.kt
@@ -1,14 +1,37 @@
 package wolfdev01.com.demowithgit
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_product.*
+import wolfdev01.com.demowithgit.adapter.ProductsAdapter
+import wolfdev01.com.demowithgit.services.DataServices
 import wolfdev01.com.demowithgit.utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataServices.getProducts(categoryType))
+        val orientation = resources.configuration.orientation
+        var spanCount = 2
+        if ( orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount =3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720){
+            spanCount = 4
+        }
+        val  layoutManager =  GridLayoutManager(this,spanCount)
+
+
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
+
     }
 }
--- app/src/main/java/wolfdev01/com/demowithgit/services/DataServices.kt
+++ app/src/main/java/wolfdev01/com/demowithgit/services/DataServices.kt
@@ -20,22 +20,32 @@ object DataServices {
     )
 
     val hats = listOf<Product>(
-            Product("Devslopes Graphic Beanie", "$18", "hat01"),
-            Product("Devslopes Graphic Black", "$20", "hat02"),
-            Product("Devslopes Graphic White", "$18", "hat03"),
-            Product("Devslopes Graphic Snapback", "$12", "hat04"))
+            Product("Devslopes Graphic Beanie", "$18", "hat1"),
+            Product("Devslopes Graphic Black", "$20", "hat2"),
+            Product("Devslopes Graphic White", "$18", "hat3"),
+            Product("Devslopes Graphic Snapback", "$12", "hat4"))
 
     val hoodies = listOf<Product>(
-            Product("Devslopes Hoodie Gray", "$28", "hoodie01"),
-            Product("Devslopes Hoodie Red", "$32", "hoodie02"),
-            Product("Devslopes Hoodie Gray", "$28", "hoodie03"),
-            Product("Devslopes Black Hoodie", "$32", "hoodie04")
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Red", "$32", "hoodie2"),
+            Product("Devslopes Hoodie Gray", "$28", "hoodie3"),
+            Product("Devslopes Black Hoodie", "$32", "hoodie4")
     )
     val shirts = listOf<Product>(
-            Product("Devslopes Shirt Black", "$18", "shirt01"),
-            Product("Devslopes Badge Light Gray", "$20", "shirt02"),
-            Product("Devslopes Logo Shirt Red", "$28", "shirt03"),
-            Product("Devslopes Hustle", "$32", "shirt04"),
-            Product("Kickflip Studio", "$12", "shirt05")
+            Product("Devslopes Shirt Black", "$18", "shirt1"),
+            Product("Devslopes Badge Light Gray", "$20", "shirt2"),
+            Product("Devslopes Logo Shirt Red", "$28", "shirt3"),
+            Product("Devslopes Hustle", "$32", "shirt4"),
+            Product("Kickflip Studio", "$12", "shirt5")
     )
+
+    val digitalGood = listOf<Product>()
+    fun getProducts(category: String): List<Product> {
+        when (category) {
+            "SHIRTS" -> return shirts
+            "HATS" -> return hats
+            "HOODIES" -> return hoodies
+            else -> return digitalGood
+        }
+    }
 }
